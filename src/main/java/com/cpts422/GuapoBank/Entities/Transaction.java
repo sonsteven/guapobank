835f0cd8efa5f15d71fce5349f50ad13687a136d
Bryce Moser <122710336+bmzr@users.noreply.github.com>
2024-09-29 16:34:56 -0700

    Add definition of Transaction entity

---
+++ src/main/java/com/cpts422/GuapoBank/Entities/Transaction.java
@@ -0,0 +1,65 @@
+package com.cpts422.GuapoBank.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private final LocalDateTime transactionDate;
+
+    @ManyToOne
+    private Account senderAccount;
+
+    @ManyToOne
+    private Account recipientAccount;
+
+    public Transaction(Long id, Double amount, LocalDateTime transactionDate, Account senderAccount, Account recipientAccount) {
+        this.id = id;
+        this.amount = amount;
+        this.transactionDate = LocalDateTime.now();
+        this.senderAccount = senderAccount;
+        this.recipientAccount = recipientAccount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public Account getSenderAccount() {
+        return senderAccount;
+    }
+
+    public void setSenderAccount(Account senderAccount) {
+        this.senderAccount = senderAccount;
+    }
+
+    public Account getRecipientAccount() {
+        return recipientAccount;
+    }
+
+    public void setRecipientAccount(Account recipientAccount) {
+        this.recipientAccount = recipientAccount;
+    }
+}
