ead4c0b45569f375a3fdfce56cf8375141995780
Bryce Moser <122710336+bmzr@users.noreply.github.com>
2024-10-30 18:47:57 -0700

    Add test coverage for Transaction.GetTransactionDate() and second Transaction constructor

--- src/test/java/com/cpts422/GuapoBank/Entities/TransactionTest.java
+++ src/test/java/com/cpts422/GuapoBank/Entities/TransactionTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import static org.mockito.Mockito.mock;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
-    private Transaction transaction;
+    private Transaction transaction, transaction2;
 
     @Mock
     private Account senderAccount;
@@ -19,9 +22,14 @@ class TransactionTest {
 
     @BeforeEach
     void setUp() {
-        transaction = new Transaction();
         senderAccount = mock(Account.class);
         recipientAccount = mock(Account.class);
+
+        // basic constructor
+        transaction = new Transaction();
+
+        // constructor with args
+        transaction2 = new Transaction(100.00d, senderAccount, recipientAccount);
     }
 
     @Test
@@ -31,15 +39,15 @@ class TransactionTest {
     }
 
     @Test
-    void TestGetAmount() {
-    }
-
-    @Test
-    void TestSetAmount() {
+    void TestGetSetAmount() {
+        transaction.setAmount(100.00d);
+        assertEquals(100.00d, transaction.getAmount());
     }
 
     @Test
     void TestGetTransactionDate() {
+        // truncate to seconds since they'll be just a bit off otherwise
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), transaction2.getTransactionDate().truncatedTo(ChronoUnit.SECONDS));
     }
 
     @Test
