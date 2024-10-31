18f9449c72b19112aa29fcd5990344cf4fd7f1a3
Bryce Moser <122710336+bmzr@users.noreply.github.com>
2024-10-30 18:21:46 -0700

    Add skeleton for TransactionTest

---
+++ src/test/java/com/cpts422/GuapoBank/Entities/TransactionTest.java
@@ -0,0 +1,42 @@
+package com.cpts422.GuapoBank.Entities;
+
+import org.junit.jupiter.api.Test;
+
+class TransactionTest {
+
+    @Test
+    void TestGetId() {
+    }
+
+    @Test
+    void TestSetId() {
+    }
+
+    @Test
+    void TestGetAmount() {
+    }
+
+    @Test
+    void TestSetAmount() {
+    }
+
+    @Test
+    void TestGetTransactionDate() {
+    }
+
+    @Test
+    void TestGetSenderAccount() {
+    }
+
+    @Test
+    void TestSetSenderAccount() {
+    }
+
+    @Test
+    void TestGetRecipientAccount() {
+    }
+
+    @Test
+    void TestSetRecipientAccount() {
+    }
+}
