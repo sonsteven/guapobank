943aa3ac9cc9d216f7f376909a5f7e4da4d7bd80
Bryce Moser <122710336+bmzr@users.noreply.github.com>
2024-10-30 21:40:55 -0700

    Complete coverage for UserTest (minus toString (not req))

--- src/test/java/com/cpts422/GuapoBank/Entities/UserTest.java
+++ src/test/java/com/cpts422/GuapoBank/Entities/UserTest.java
@@ -2,16 +2,34 @@ package com.cpts422.GuapoBank.Entities;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserTest {
 
     private User user;
+    private User user2 = new User();
+    private User user3 = new User("TestUser", "password",
+            "UserFirstName", "UserLastName", "User");
+
+    @Mock
+    private Account account;
+
+    private List<Account> accounts;
+
     @BeforeEach
     void setUp() {
         user = new User("TestUser", "password",
-                "UserFirstName", "UserLastName", "User", false, false, false);
+                "UserFirstName", "UserLastName", "User",
+                false, false, false);
+
+        account = mock(Account.class);
+        accounts = new ArrayList<>();
     }
 
     @Test
@@ -22,34 +40,45 @@ class UserTest {
 
     @Test
     void TestAddAccount() {
+        user.addAccount(account);
+        assertTrue(user.getAccounts().contains(account));
+        verify(account, times(1)).setUser(user);
     }
 
     @Test
     void TestGetSetAccounts() {
+        user.setAccounts(accounts);
+        assertEquals(accounts, user.getAccounts());
     }
 
     @Test
     void TestGetSetPassword() {
+        user.setPassword("pa55w0rd");
+        assertEquals("pa55w0rd", user.getPassword());
     }
 
     @Test
     void TestGetSetUsername() {
+        user.setUsername("testUserrrr");
+        assertEquals("testUserrrr", user.getUsername());
     }
 
     @Test
     void TestGetSetFirstName() {
+        user.setFirstName("Bryce");
+        assertEquals("Bryce", user.getFirstName());
     }
 
     @Test
     void TestGetSetLastName() {
+        user.setLastName("Moser");
+        assertEquals("Moser", user.getLastName());
     }
 
     @Test
     void TestGetSetRole() {
-    }
-
-    @Test
-    void createTransaction() {
+        user.setRole("Admin");
+        assertEquals("Admin", user.getRole());
     }
 
 //    @Test
@@ -58,14 +87,20 @@ class UserTest {
 
     @Test
     void TestIsSetVip() {
+        user.setVip(true);
+        assertTrue(user.isVip());
     }
 
     @Test
     void TestIsSetMilitary() {
+        user.setMilitary(true);
+        assertTrue(user.isMilitary());
     }
 
     @Test
     void TestIsSetCorporate() {
+        user.setCorporate(true);
+        assertTrue(user.isCorporate());
     }
 
 }
