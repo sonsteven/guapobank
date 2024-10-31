132c074fa9eede55c3a0d5f034f4c19c31bc53d6
Bryce Moser <122710336+bmzr@users.noreply.github.com>
2024-10-30 18:38:52 -0700

    Add test coverage for Transaction.GetId() and Transaction.SetId()

--- src/test/java/com/cpts422/GuapoBank/Entities/TransactionTest.java
+++ src/test/java/com/cpts422/GuapoBank/Entities/TransactionTest.java
@@ -1,15 +1,33 @@
 package com.cpts422.GuapoBank.Entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
-    @Test
-    void TestGetId() {
+    private Transaction transaction;
+
+    @Mock
+    private Account senderAccount;
+
+    @Mock
+    private Account recipientAccount;
+
+    @BeforeEach
+    void setUp() {
+        transaction = new Transaction();
+        senderAccount = mock(Account.class);
+        recipientAccount = mock(Account.class);
     }
 
     @Test
-    void TestSetId() {
+    void TestGetSetId() {
+        transaction.setId(1L);
+        assertEquals(1L, transaction.getId());
     }
 
     @Test
